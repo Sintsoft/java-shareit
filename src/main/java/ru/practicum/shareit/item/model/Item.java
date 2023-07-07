913e1dc1769fb74a0f72f5126a66c49a931d6ec9
Oleg Sintsov <sintsov.o.yu@yandex.ru>
2023-07-07 21:54:05 +0300

    fix: I hope it change something

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -11,7 +11,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Data
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Table(name = "items", schema = "shareit")
