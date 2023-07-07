20eb7285ba8e55f9ac345e5b828cb3c3932e570b
Oleg Sintsov <sintsov.o.yu@yandex.ru>
2023-07-07 16:30:36 +0300

    fix: Exception catcher update

--- src/main/java/ru/practicum/shareit/utility/errorHandling/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/utility/errorHandling/ErrorHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.utility.errorHandling.exceptions.*;
 
-import static com.sun.tools.sjavac.Util.getStackTrace;
+import java.util.Arrays;
 
 
 @Slf4j
@@ -60,6 +60,6 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse otherExceprions(Throwable ex) {
         log.info("Level: ERROR HANDLER. Catched exception: " + ex.getClass() + ". Message: " + ex.getMessage());
-        return new ErrorResponse(ex.getClass() + "        " + ex.getMessage() + "        " + getStackTrace(ex));
+        return new ErrorResponse(ex.getClass() + "        " + ex.getMessage() + "        " + Arrays.toString(ex.getStackTrace()));
     }
 }
