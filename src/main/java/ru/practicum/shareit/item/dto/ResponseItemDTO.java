defba6f657aaaec654e961045976c0c76808f4e2
Oleg Sintsov <sintsov.o.yu@yandex.ru>
2023-07-08 22:13:22 +0300

    refactor: ResponseItemDTO class

--- src/main/java/ru/practicum/shareit/item/dto/ResponseItemDTO.java
+++ src/main/java/ru/practicum/shareit/item/dto/ResponseItemDTO.java
@@ -4,6 +4,7 @@ package ru.practicum.shareit.item.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.shareit.request.dto.NestedItemRequestDTO;
 
 @Data
 @AllArgsConstructor
@@ -17,4 +18,6 @@ public class ResponseItemDTO {
     private String description;
 
     private Boolean available;
+
+    private Long requestId;
 }
